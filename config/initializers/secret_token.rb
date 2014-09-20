00c4b5288ff3a166e9a237d79fcd6694af8ed203
takecian <fujikidev@gmail.com>
2014-09-21 01:03:44 +0900

    add secrets.yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JapanIsCool::Application.config.secret_key_base = '152a4fa4928cd4e752426e13ada4f0334ad068be918c0ab714d9cf17da9eb79ac8a19c46a505c046ab951ab5e7ed3c71b91e274b25fce894ff4e42e3bc9edb6d'
