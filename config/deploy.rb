9a9b318236ac1d2a677a26621b0d7e8258a6c9fc
takecian <fujikidev@gmail.com>
2014-09-21 00:16:50 +0900

    add deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,35 +1,43 @@
 set :application, 'JapanIsCool'
 set :repo_url, 'git@bitbucket.org:takecian/japaniscool.git'
 
+set :ssh_options, { forward_agent: true }
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 set :branch, 'master'
 
+# Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/var/www/app'
+
+# Default value for :scm is :git
 set :scm, :git
 
 set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 
-set :format, :pretty
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
 set :log_level, :debug
-set :pty, true
 
-set :git_shallow_clone, 1
+# Default value for :pty is false
+# set :pty, true
 
+# Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-set :keep_releases, 3
-
-# for capistrano v3 no need to call following command. it's called by default
-#set :whenever_command, "bundle exec whenever"
-#require 'whenever/capistrano'
-set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
-
 set :linked_dirs, (fetch(:linked_dirs) + ['tmp/pids'])
 
 set :unicorn_rack_env, "none"
-set :unicorn_config_path, 'config/unicorn.rb'
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
